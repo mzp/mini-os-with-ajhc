5936e10fdfe43d36810f36022f3ee02d65df7ac6
mzp <mzpppp@gmail.com>
2013-08-11 12:27:26 +0000

    Remove extra output and import

--- libjhcrts/Main.hs
+++ libjhcrts/Main.hs
@@ -1,8 +1,2 @@
-import Jhc.Prim.Rts
-import Time
-import Util
-
 main :: IO ()
-main = do printk "***************\n"#
-          printk "hello from ajhc\n"#
-          printk "***************\n"#
+main = return ()
